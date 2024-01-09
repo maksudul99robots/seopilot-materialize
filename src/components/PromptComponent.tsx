7ac4d1d39105ed1a597f7e695c2e281d711610fa
mtushar78 <mtushar78@gmail.com>
2024-01-10 02:55:40 +0600

    generated outlines are shown

--- src/components/PromptComponent.tsx
+++ src/components/PromptComponent.tsx
@@ -5,10 +5,12 @@ import { LoginRegistrationAPI } from "src/services/API"
 export const PromptComponent = (props: any) => {
     const [outlinePrompt, setOutlinePrompt] = useState('');
     const [articlePrompt, setArticlePrompt] = useState('');
+    const [outlines, setOutlines] = useState('');
     useEffect(() => {
         LoginRegistrationAPI.getPrompt({ id: props.id }).then(res => {
             setOutlinePrompt(res.data.outline_prompt)
             setArticlePrompt(res.data.article_prompt)
+            setOutlines(res.data.generated_outlines)
         }).catch(e => {
             console.log(e)
         })
@@ -19,6 +21,10 @@ export const PromptComponent = (props: any) => {
             <Typography variant="h5" sx={{ marginBottom: "10px" }}>Outline Prompt</Typography>
             <Typography id="modal-description" sx={{ whiteSpace: 'pre-line' }}>
                 {outlinePrompt}
+            </Typography>
+            <Typography variant="h5" sx={{ marginBottom: "10px" }}>Generated Outlines</Typography>
+            <Typography id="modal-description" sx={{ whiteSpace: 'pre-line' }}>
+                {outlines}
             </Typography>
             <Typography variant="h5" sx={{ marginBottom: "10px", marginTop: "10px" }}>Article Prompt</Typography>
             <Typography id="modal-description" sx={{ whiteSpace: 'pre-line' }}>
