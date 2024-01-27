3c89cf8628eef8ee09e64d2f85f8bb0523da83e8
sumayachandona <sumayachandona5549@gmail.com>
2024-01-27 12:47:10 +0600

    wordpress connect showed on admin details

---
+++ src/pages/generated-article/[id]/InsertImage.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react"
+
+export const InsertImage = (props: any) => {
+    const [imgSrc, setImgSrc] = useState('');
+
+
+    return (
+        <div style={{ display: "flex", justifyContent: "center", marginBottom: "20px", overflow: "hidden" }}>
+            {
+                props.service == 'unsplash' && props.fImg?.user?.links ?
+                    <div style={{ width: "800px", height: "450px", marginBottom: "40px", }}>
+                        <img
+                            src={imgSrc}
+                            width={800}
+                            height={450}
+                            style={{ objectFit: "cover" }}
+                            alt="Featured image"
+                        />
+                        <p style={{ fontSize: "12px", fontWeight: 400, textAlign: "center", marginTop: "0px" }}>
+                            Photo by <a href={props.fImg.user.links.html + "?utm_source=Seopilot&utm_medium=referral"} target='_blank' className='colorLink'>{props.fImg.user.name}</a> on <a href='https://unsplash.com/?utm_source=Seopilot&utm_medium=referral' target='_blank' className='colorLink'>Unsplash</a>
+                        </p>
+                    </div>
+                    :
+                    <div style={{ width: "800px", height: "450px", marginBottom: "40px", }}>
+                        <img
+                            src={imgSrc}
+                            width={800}
+                            height={450}
+                            style={{ objectFit: "cover" }}
+                            alt="Featured image"
+                        />
+                        <p style={{ fontSize: "12px", fontWeight: 400, textAlign: "center", marginTop: "0px" }}>
+                            Photo by <a href={props.fImg.user.links.html + "?utm_source=Seopilot&utm_medium=referral"} target='_blank' className='colorLink'>{props.fImg.user.name}</a> on <a href='https://unsplash.com/?utm_source=Seopilot&utm_medium=referral' target='_blank' className='colorLink'>Unsplash</a>
+                        </p>
+                    </div>
+            }
+
+
+
+        </div>
+    )
+}
